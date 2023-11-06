2162c922552f13f82c14d69b5c76e73a6499e903
Sikxor <69762292+WolffCorentin@users.noreply.github.com>
2023-11-06 14:56:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import requests
 
-for i in range(1,500):
-  r = requests.get(url="https://punapi.rest/api/pun")
-  print(r.content)
+r = requests.get(url="https://punapi.rest/api/pun")
+print(r.content)
